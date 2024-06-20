5f98dafe39bc9229ae9732ba6f2c9bdf66286aef
s4016080-ps1nov <s4016080@student.rmit.edu.au>
2024-06-20 17:47:29 +1000

    add/doxygen-deploy

---
+++ configure.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+echo "Cloning the repository and checking out the gh-pages branch..."
+git clone --branch=gh-pages https://x-access-token:${GITHUB_TOKEN}@github.com/${{ github.repository }} gh-pages
+cd gh-pages
+
+echo "Cleaning up the old files..."
+find . -maxdepth 1 ! -name '.git' ! -name '.' -exec rm -rf {} +
+
+echo "Copying the new documentation..."
+cp -r ../doc/html/* .
+
+echo "Adding changes..."
+git add .
+echo "Check if there's changes..."
+if git diff-index --quiet HEAD; then
+    echo "No changes to commit."
+    exit 0
+fi
+
+echo "Committing changes..."
+git commit -m "Deploy updated documentation"
+
+echo "Pushing changes to gh-pages branch..."
+git push origin gh-pages --force
+
