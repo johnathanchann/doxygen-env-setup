bde5690008787112342028b4e00391f515fdcaae
Johnathan Chan <67891519+Jinxto@users.noreply.github.com>
2024-06-21 10:19:28 +1000

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
-echo "Cloning the repository and checking out the gh-pages branch..."
-git clone --branch=gh-pages https://x-access-token:${GITHUB_TOKEN}@github.com/${{ github.repository }} gh-pages
-cd gh-pages
+echo "Cloning the repository..."
+git clone https://x-access-token:${GITHUB_TOKEN}@github.com/${{ github.repository }} repo
+cd repo
+
+# Check if the gh-pages branch exists
+if git show-ref --verify --quiet refs/heads/gh-pages; then
+    echo "Switching to gh-pages branch..."
+    git checkout gh-pages
+else
+    echo "Creating gh-pages branch..."
+    git checkout --orphan gh-pages
+    git rm -rf .
+    touch .gitkeep
+    git add .gitkeep
+    git commit -m "Initial commit on gh-pages branch"
+    git push origin gh-pages
+fi
 
 echo "Cleaning up the old files..."
 find . -maxdepth 1 ! -name '.git' ! -name '.' -exec rm -rf {} +
@@ -11,9 +25,10 @@ cp -r ../doc/html/* .
 
 echo "Adding changes..."
 git add .
+echo "Check if there's changes..."
 if git diff-index --quiet HEAD; then
-   echo "No changes to commit."
-   exit 0
+    echo "No changes to commit."
+    exit 0
 fi
 
 echo "Committing changes..."
@@ -21,3 +36,5 @@ git commit -m "Deploy updated documentation"
 
 echo "Pushing changes to gh-pages branch..."
 git push origin gh-pages --force
+
+echo "Done"
